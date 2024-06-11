82ec47efe739cec82c6b5fc4bb484038f1d11b3c
KeisukeFuruta <kcomethunter@gmail.com>
2024-06-11 15:18:21 +0900

    コメントを整理

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,20 +3,29 @@ package org.example;
 import java.util.Scanner;
 import java.util.Objects;
 
+// Greeting と Speakingは使わずにinterfaceと実装を組み合わせて作ってみてください
+// interfaceの実装を二つ以上作ってください。
+// 継承を使ってみてください。extends 上記で作った二つの実装クラスのどちらかを継承してクラスを作って実際にメソッドを呼び出してみてください。
+// メソッド呼び出しは親子のどちらとも呼び出してみてください。
+
 public class Main {
+  // ポケモンのinterfaceを作る。要素は・戦う・逃げる
+  // classは3つ、カイロス、ヒトカゲ、リザードン
 
   public static String selectedPokemon;
 
   public static void main(String[] args) {
-
+    // mainでは、敵との遭遇からスタート、呼び出すポケモン(class)を選び、対戦スタート。
     Scanner scanner = new Scanner(System.in);
     Pokemon selectedPokemonInstance = null;
 
+    //while文で正しい名前のポケモンが選ばれるまでループさせる
     while (selectedPokemonInstance == null) {
       System.out.println("野生のカビゴンが現れた！あなたはどのポケモンで戦う？");
       System.out.println("（カイロス/ヒトカゲ/リザードン）から選んでね");
       selectedPokemon = scanner.nextLine();
 
+      //元々if文で記述していたが、switch文に置換。スッキリ読みやすくなった。
       switch (selectedPokemon) {
         case "カイロス" -> selectedPokemonInstance = new Pinsir();
         case "ヒトカゲ" -> selectedPokemonInstance = new Charmander();
@@ -25,9 +34,13 @@ public class Main {
       }
     }
 
+    //　バトル開始。選択したポケモンによって名前が変わるように変数を定義。
+    //　親クラス（ヒトカゲクラス）で使用した「ひのこ」をリザードンでも共通で使いたく、
+    // selectedPokemonはクラスフィールドとして宣言した
     System.out.println("いけっ" + selectedPokemon + "!");
     selectedPokemonInstance.battle();
 
+    //リザードン以外は敗走ルート。runAwayメソッドを使用して逃げる。
     if (Objects.equals(selectedPokemon, "リザードン")) {
       System.out.println("カビゴンを倒した！おめでとう");
     } else {
@@ -36,12 +49,3 @@ public class Main {
     }
   }
 }
-
-// Greeting と Speakingは使わずにinterfaceと実装を組み合わせて作ってみてください
-// interfaceの実装を二つ以上作ってください。
-// 継承を使ってみてください。extends 上記で作った二つの実装クラスのどちらかを継承してクラスを作って実際にメソッドを呼び出してみてください。
-// メソッド呼び出しは親子のどちらとも呼び出してみてください。
-// ポケモンのinterfaceを作る。要素は・戦う・逃げる
-// classは3つ、カイロス、ヒトカゲ、リザードン
-// mainでは、敵との遭遇からスタート、呼び出すポケモン(class)を選び、対戦スタート。
-//
